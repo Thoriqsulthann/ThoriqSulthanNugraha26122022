a5ecf42d9d9a62b5c6726fad2669bae3b225d486
Thoriqsulthann <118926068+Thoriqsulthann@users.noreply.github.com>
2023-01-09 00:27:23 +0700

    Create BukuDao.java

---
+++ model/BukuDao.java
@@ -0,0 +1,8 @@
+import java.util.List;
+public interface BukuDao {
+    void save(Buku buku);
+    void update(int index,Buku buku);
+    void delete (int index); 
+    Buku getBuku(int index);
+    List<Buku>getAll();
+}
