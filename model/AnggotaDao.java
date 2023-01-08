f772ff6beec8d9cb06c578bbb0e5b500e72efbd7
Thoriqsulthann <118926068+Thoriqsulthann@users.noreply.github.com>
2023-01-09 00:25:28 +0700

    Create AnggotaDao.java

---
+++ model/AnggotaDao.java
@@ -0,0 +1,8 @@
+import java.util.List;
+public interface AnggotaDao {
+    void save(Anggota anggota);
+    void update(int index, Anggota anggota);
+    void delete (int index); 
+    Anggota getAnggota(int index);
+    List<Anggota>getAll();
+}
